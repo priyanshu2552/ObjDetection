07d3ab53c475b183ffa58f2bb7095786de9489fe
Priyanshu Kumar Raj <95175056+priyanshu2552@users.noreply.github.com>
2025-03-16 03:46:06 +0530

    Update server.js
    frontend url added to cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://your-frontend-project.vercel.app']
+    origin: ['https://objdetection-3.onrender.com']
 }));
 app.use(express.json());
 app.use(express.static("uploads"));
